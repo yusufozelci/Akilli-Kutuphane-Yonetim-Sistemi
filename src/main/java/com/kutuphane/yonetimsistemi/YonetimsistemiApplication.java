96ff89144a419b99393603fd64c38da9a0fb0f71
Yusuf Özelci <yusuf.ozelci@hotmail.com>
2025-10-12 21:32:54 +0300

    Spring Boot proje iskeleti kuruldu, veritabanı bağlantısı yapıldı

---
+++ src/main/java/com/kutuphane/yonetimsistemi/YonetimsistemiApplication.java
@@ -0,0 +1,13 @@
+package com.kutuphane.yonetimsistemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YonetimsistemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YonetimsistemiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kutuphane/yonetimsistemi/YonetimsistemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutuphane.yonetimsistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonetimsistemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
