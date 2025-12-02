08c7ebb4179eb40dbb6707c5e528f9e0f1847bcd
Yusuf Özelci <yusuf.ozelci@hotmail.com>
2025-12-02 22:57:05 +0300

    Entity sınıfları düzenlendi.

--- src/main/java/com/kutuphane/yonetimsistemi/entity/Kitap.java
+++ src/main/java/com/kutuphane/yonetimsistemi/entity/Kitap.java
@@ -15,7 +15,7 @@ public class Kitap {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Integer id;
 
     @Column(name = "kitap_adi", nullable = false)
     private String kitapAdi;
