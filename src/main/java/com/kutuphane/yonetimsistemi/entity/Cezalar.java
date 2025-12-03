980b42f670b2f9c94a73087ac6fc9e42a961ce48
Yusuf Özelci <yusuf.ozelci@hotmail.com>
2025-12-03 21:34:11 +0300

    Cezalara gecikme süresi hesaplama mantığı (@Transient) eklendi.

--- src/main/java/com/kutuphane/yonetimsistemi/entity/Cezalar.java
+++ src/main/java/com/kutuphane/yonetimsistemi/entity/Cezalar.java
@@ -1,9 +1,11 @@
 package com.kutuphane.yonetimsistemi.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
+import java.time.temporal.ChronoUnit;
 
 @Entity
 @Table(name = "Cezalar")
@@ -21,10 +23,22 @@ public class Cezalar {
     @JoinColumn(name = "odunc_alma_id", nullable = false, unique = true)
     private OduncAlma oduncAlma;
 
-
     @Column(name = "ceza_tutari", nullable = false, precision = 10, scale = 2)
     private BigDecimal cezaTutari;
 
     @Column(name = "odendi_mi", nullable = false)
     private boolean odendiMi = false;
+
+    @Transient
+    public long getGecikmeSuresi() {
+        if (oduncAlma != null &&
+                oduncAlma.getSonIadeTarihi() != null &&
+                oduncAlma.getGercekIadeTarihi() != null) {
+            return ChronoUnit.DAYS.between(
+                    oduncAlma.getSonIadeTarihi(),
+                    oduncAlma.getGercekIadeTarihi()
+            );
+        }
+        return 0;
+    }
 }
